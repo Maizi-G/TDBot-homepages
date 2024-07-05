52eb36386ddfa78f14b0abbb329bba6c184cfbca
Maizi-G <maizi@mzchn.cn>
2024-07-05 23:24:58 +0800

    V1.0.3提交

---
+++ .vitepress/config.js
@@ -0,0 +1,23 @@
+export default {
+    lang: 'zh-CN',
+    title: 'TD Bot',
+    description: '智能的开放公益机器人',
+
+    lastUpdated: true,
+
+    themeConfig: {
+        logo: '/logo.png',
+
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '隐私政策', link: '/privacy-policy.md' },
+            { text: '关于', link: '/about.md' },
+        ],
+
+
+        footer: {
+            message: '以 MIT 许可协议发布',
+            copyright: '版权所有 © 2021-2024 TD科技中心'
+        },
+    }
+}
